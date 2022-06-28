2ef76e0f105c9a6a9f84a8a998f16e1b9ba27533
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-06-28 19:20:56 +0530

    home final commit

---
+++ script/buttons.js
@@ -0,0 +1,6 @@
+var cvBtn = document.querySelector(".cv-btn");
+
+cvBtn.addEventListener("click", () => {
+    // window.location = "../documents/Gaurav Kumar Tripathi.pdf";
+    window.open("../documents/Gaurav Kumar Tripathi.pdf");
+});

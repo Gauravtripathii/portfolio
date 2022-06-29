9f40cece3341543332f51db14458f43cbd266823
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-06-29 19:25:30 +0530

    cv update

--- script/buttons.js
+++ script/buttons.js
@@ -2,7 +2,7 @@ var cvBtn = document.querySelector(".cv-btn");
 
 cvBtn.addEventListener("click", () => {
   // window.location = "../documents/Gaurav Kumar Tripathi.pdf";
-  window.open("../documents/Gaurav Kumar Tripathi.pdf");
+  window.open("https://drive.google.com/file/d/1Gs2Byoq-DSccmuyTK5Pb-f6qlCI112hM/view?usp=sharing");
 });
 
 var exp1 = document.querySelector(".view-exp-1");

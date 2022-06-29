3a2e3d14d937afd434d7c483e42894d72e934d08
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-06-29 09:09:00 +0530

    .

--- script/buttons.js
+++ script/buttons.js
@@ -1,6 +1,12 @@
 var cvBtn = document.querySelector(".cv-btn");
 
 cvBtn.addEventListener("click", () => {
-    // window.location = "../documents/Gaurav Kumar Tripathi.pdf";
-    window.open("../documents/Gaurav Kumar Tripathi.pdf");
+  // window.location = "../documents/Gaurav Kumar Tripathi.pdf";
+  window.open("../documents/Gaurav Kumar Tripathi.pdf");
+});
+
+var exp1 = document.querySelector(".view-exp-1");
+
+exp1.addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1cgpXBWRBuI-IX0xDLLA-Rl-_DALxedZE/view?usp=sharing");
 });

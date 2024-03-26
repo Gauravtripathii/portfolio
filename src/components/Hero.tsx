474476fa43a44b7cc75dfe18a4f32a0bae2af97c
gauravtripathii <gauravtripathii>
2024-03-26 22:46:09 +0530

    Hero init

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const positions = ["Frontend", "Backend", "Fullstack"];
+
+function Hero() {
+  const [position, setPosition] = useState<string>(positions[0]);
+  useEffect(() => {
+    const timeoutId = setInterval(() => {
+      const nextPositionIndex =
+        (positions.indexOf(position) + 1) % positions.length;
+      setPosition(positions[nextPositionIndex]);
+    }, 1500);
+    return () => clearInterval(timeoutId);
+  }, [position]);
+
+  return (
+    <div className="w-full h-5/6 p-10">
+      {/* positions intro */}
+      <div className="text-white text-6xl font-semibold flex flex-col gap-2">
+        <span>
+          A <span className="text-red-500">{position}</span>
+        </span>
+        <span>Developer.</span>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 function Homepage() {
   return (
     <div className="w-full h-full">
       <Header />
+      <Hero />
     </div>
   );
 }

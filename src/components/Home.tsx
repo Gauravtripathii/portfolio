8093c5dab0a35b5e9dc5173bd23860cb19049c91
gauravtripathii <gauravtripathii>
2024-03-27 17:06:00 +0530

    Hero.tsx, Home.tsx responsive final

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,35 +14,37 @@ function Home() {
   }, [position]);
 
   return (
-    <div className="w-full h-5/6 p-10 flex flex-col gap-10">
+    <div className="w-full h-5/6 p-10 flex flex-col gap-10 lg:flex-row">
       {/* positions intro */}
-      <div className="text-red-50 text-6xl font-semibold flex flex-col gap-2">
+      <div className="text-red-50 text-6xl font-semibold flex flex-col gap-2 lg:w-3/4 lg:items-center lg:justify-center lg:text-7xl xl:text-9xl">
         <span>
           A <span className="text-red-500">{position}</span>
         </span>
         <span>Developer.</span>
+
+        <div className="hidden lg:block text-red-50 text-xl mt-10 xl:text-3xl">Clean Code. Elegant Design.</div>
       </div>
 
       {/* redirections */}
-      <div className="h-full flex flex-col items-center justify-center gap-2 text-red-50 text-xl">
+      <div className="h-full flex flex-col items-center justify-center gap-2 text-red-50 text-xl lg:w-1/4 xl:text-3xl">
         {/* resume */}
-        <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition">
+        <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition cursor-pointer">
           <span>Resume</span>
           <span>&rarr;</span>
         </div>
 
         {/* socials */}
-        <div className="text-slate-400 mt-5 w-full font-mono">SOCIALS</div>
-        <div className="w-full flex flex-col gap-3">
-          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition">
+        <div className="text-slate-400 mt-5 w-full font-mono xl:mt-14">SOCIALS</div>
+        <div className="w-full flex flex-col gap-3 xl:gap-7">
+          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
             <span>LinkedIn</span>
             <span>&rarr;</span>
           </div>
-          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition">
+          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
             <span>X</span>
             <span>&rarr;</span>
           </div>
-          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition">
+          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
             <span>FrontendMentor</span>
             <span>&rarr;</span>
           </div>

892920a9d267d8b3d7df215d3a20438a62da2815
gauravtripathii <gauravtripathii>
2024-04-01 13:27:10 +0530

    framer-motion animations for the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,37 @@ import hamburger_icon from "../assets/menu.png";
 
 import { NavLink } from "react-router-dom";
 
+import { motion } from "framer-motion";
+
+interface variantsType {
+  open: { opacity: number; x?: string; y?: string; };
+  closed: { opacity: number; x?: string; y?: string; };
+  openT: { opacity: number; x?: string; y?: string; };
+  closedT: { opacity: number; x?: string; y?: string; };
+}
+
+const smallScreenVariants: variantsType = {
+  open: { opacity: 1, x: "0" },
+  closed: { opacity: 0, x: "100%" },
+  openT: { opacity: 0.75, x: "0" },
+  closedT: { opacity: 0, x: "100%" },
+};
+
+const largeScreenVariants: variantsType = {
+  open: { opacity: 1, y: "0" },
+  closed: { opacity: 0, y: "100%" },
+  openT: { opacity: 0.75, y: "0" },
+  closedT: { opacity: 0, y: "100%" },
+};
+
 function Header() {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [variants, setVariants] = useState<variantsType>(smallScreenVariants);
+
+  window.onresize = () =>
+    window.innerWidth < 1024
+      ? setVariants(smallScreenVariants)
+      : setVariants(largeScreenVariants);
   return (
     <header className="h-1/6 w-full border-b flex items-center justify-between px-10 z-20">
       {/* logo */}
@@ -27,17 +56,18 @@ function Header() {
       </div>
 
       {/* menu */}
-      <div
-        className={`w-full h-1/4 lg:w-1/2 lg:h-full bg-black absolute top-0 left-0 opacity-75 flex justify-center items-center cursor-pointer ${
-          menuOpen ? "block" : "hidden"
-        }`}
+      <motion.div
+        animate={menuOpen ? "openT" : "closedT"}
+        variants={variants}
+        transition={{ ease: "easeOut", duration: 0.1 }}
+        className={`w-full h-1/4 lg:w-1/2 lg:h-full bg-black absolute top-0 left-0 flex justify-center items-center cursor-pointer`}
         onClick={() => setMenuOpen(!menuOpen)}
-      ></div>
+      ></motion.div>
 
-      <div
-        className={`w-full h-3/4 lg:w-1/2 lg:h-full bg-red-50 absolute bottom-0 right-0 flex flex-col items-center justify-center gap-10 text-2xl font-semibold text-[#050A18] lg:gap-14 ${
-          menuOpen ? "block" : "hidden"
-        }`}
+      <motion.div
+        animate={menuOpen ? "open" : "closed"}
+        variants={variants}
+        className={`w-full h-3/4 lg:w-1/2 lg:h-full bg-red-50 absolute bottom-0 right-0 flex flex-col items-center justify-center gap-10 text-2xl font-semibold text-[#050A18] lg:gap-14 `}
       >
         <NavLink
           to="/"
@@ -59,7 +89,7 @@ function Header() {
         >
           Work
         </NavLink>
-      </div>
+      </motion.div>
     </header>
   );
 }

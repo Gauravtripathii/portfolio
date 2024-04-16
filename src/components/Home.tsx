af8d4b5adbb42c56ce67ceee1e3a29d1ec955e04
gauravtripathii <gauravtripathii>
2024-04-16 21:22:21 +0530

    Home.tsx animation added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import { motion } from "framer-motion";
+
 const positions = ["Frontend", "Backend", "Fullstack"];
 
 function Home() {
@@ -17,12 +19,20 @@ function Home() {
     <div className="w-full h-5/6 p-10 flex flex-col gap-10 lg:flex-row">
       {/* positions intro */}
       <div className="text-red-50 text-6xl sm:text-8xl font-semibold flex flex-col gap-2 lg:w-3/4 lg:items-center lg:justify-center lg:text-7xl xl:text-9xl">
-        <span>
+        <motion.div initial={{ x: "-100%" }} animate={{ x: 0 }}>
           A <span className="text-red-500">{position}</span>
-        </span>
-        <span>Developer.</span>
+        </motion.div>
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.1 }}
+        >
+          Developer.
+        </motion.div>
 
-        <div className="hidden lg:block text-red-50 text-xl mt-10 xl:text-3xl">Clean Code. Elegant Design.</div>
+        <motion.div initial={{ x: "-100%" }} animate={{ x: 0 }} className="hidden lg:block text-red-50 text-xl mt-10 xl:text-3xl">
+          Clean Code. Elegant Design.
+        </motion.div>
       </div>
 
       {/* redirections */}
@@ -34,7 +44,9 @@ function Home() {
         </div>
 
         {/* socials */}
-        <div className="text-slate-400 mt-5 w-full font-mono xl:mt-14 xl:mb-5">SOCIALS</div>
+        <div className="text-slate-400 mt-5 w-full font-mono xl:mt-14 xl:mb-5">
+          SOCIALS
+        </div>
         <div className="w-full flex flex-col gap-3 xl:gap-7">
           <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
             <span>LinkedIn</span>

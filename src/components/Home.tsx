ff2f1f5e0e08fc2c9dbeb84e0162357ffe79ed4b
gauravtripathii <gauravtripathii>
2024-04-20 21:07:49 +0530

    resume and social links updated

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 import { motion } from "framer-motion";
 
+import resumeUrl from "../assets/gaurav_kumar_tripathi.pdf";
+
 const positions = ["Frontend", "Backend", "Fullstack"];
 
 function Home() {
@@ -14,6 +16,19 @@ function Home() {
     }, 1500);
     return () => clearInterval(timeoutId);
   }, [position]);
+
+  const openLink = (link: string) => {
+    window.open(link, "_blank");
+  };
+  const openResumeAndDownload = (link: string) => {
+    openLink(link);
+    const resume_link = document.createElement("a");
+    resume_link.href = resumeUrl;
+    resume_link.download = "gaurav_kumar_tripathi.pdf";
+    document.body.appendChild(resume_link);
+    resume_link.click();
+    document.body.removeChild(resume_link);
+  };
 
   return (
     <div className="w-full h-5/6 p-10 flex flex-col gap-10 lg:flex-row">
@@ -30,7 +45,11 @@ function Home() {
           Developer.
         </motion.div>
 
-        <motion.div initial={{ x: "-100%" }} animate={{ x: 0 }} className="hidden lg:block text-red-50 text-xl mt-10 xl:text-3xl">
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{ x: 0 }}
+          className="hidden lg:block text-red-50 text-xl mt-10 xl:text-3xl"
+        >
           Clean Code. Elegant Design.
         </motion.div>
       </div>
@@ -38,7 +57,14 @@ function Home() {
       {/* redirections */}
       <div className="h-full flex flex-col items-center justify-center gap-2 text-red-50 text-xl lg:w-1/4 xl:text-3xl">
         {/* resume */}
-        <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition cursor-pointer">
+        <div
+          className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition cursor-pointer"
+          onClick={() =>
+            openResumeAndDownload(
+              "https://docs.google.com/document/d/1YRlA69avCSaBQMRYaCEMmnTXKftBfKG2NEpdvr__j14/edit?usp=sharing"
+            )
+          }
+        >
           <span>Resume</span>
           <span>&rarr;</span>
         </div>
@@ -47,17 +73,28 @@ function Home() {
         <div className="text-slate-400 mt-5 w-full font-mono xl:mt-14 xl:mb-5">
           SOCIALS
         </div>
-        <div className="w-full flex flex-col gap-3 xl:gap-7">
+        <div
+          className="w-full flex flex-col gap-3 xl:gap-7"
+          onClick={() =>
+            openLink("https://www.linkedin.com/in/gaurav-kumar-tripathii/")
+          }
+        >
           <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
             <span>LinkedIn</span>
             <span>&rarr;</span>
           </div>
-          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
+          <div
+            className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer"
+            onClick={() => openLink("https://twitter.com/gaurav0tripathi")}
+          >
             <span>X</span>
             <span>&rarr;</span>
           </div>
-          <div className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer">
-            <span>FrontendMentor</span>
+          <div
+            className="border-b w-full flex items-center justify-between px-1 hover:text-red-500 hover:border-red-500 hover:-translate-y-1 transition  cursor-pointer"
+            onClick={() => openLink("https://github.com/gauravtripathii")}
+          >
+            <span>GitHub</span>
             <span>&rarr;</span>
           </div>
         </div>
